47c0cda80a27a147ff0658436616f82731ccec5a
shipra5676 <68421390+shipra5676@users.noreply.github.com>
2024-11-15 15:36:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This is the default for CRA apps
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
